370df74f368a03070580aabf4f892d809c2ac1b8
ssuvorov <suvorov@findmykids.org>
2022-11-08 01:00:46 +0500

    Add templating

--- app/views.py
+++ app/views.py
@@ -1,18 +1,27 @@
 from flask import Blueprint, render_template
 
 from .forms import ConfigForm
+from .functions import parse_form_into_json
 
 
 views = Blueprint('views', __name__)
 
 
 @views.route('/', methods=['GET', 'POST'])
-def main() -> str:
-    class GrpupsSubform(ConfigForm):
-        pass
+def form() -> str:
+    form = ConfigForm()
 
-    form = GrpupsSubform()
-    return render_template('main.html', form=form)
+    if form.validate_on_submit():
+        # form_json = parse_form_into_json(form)
+        # return render_template('config.html', form_json=form_json)
+        return render_template('config.html')
+
+    return render_template('form.html', form=form)
+
+
+@views.route('/config')
+def config() -> str:
+    return render_template('config.html')
 
 
 @views.route('/tmp', methods=['GET', 'POST'])
